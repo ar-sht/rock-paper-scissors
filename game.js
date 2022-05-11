3806bc14e79037b2a56f50bc307b46a28758afae
Ari Shtein <ashtein120@gmail.com>
2022-05-10 22:50:38 -0400

    gui is sucky but it's functional and even stops after the game's over

--- game.js
+++ game.js
@@ -1,67 +1,48 @@
-const PLAYS = Array("Rock", "Paper", "Scissors")
+const moves = {
+    0: "Paper",
+    1: "Scissors",
+    2: "Rock"
+}
 
 function computerPlay() {
-    return PLAYS[Math.floor(Math.random()*3)]
+    return Math.floor(Math.random()*3)
 }
 
-function displayResult(message) {
-    document.getElementById("results").innerHTML += message
-    return
-}
+document.getElementById("rock").addEventListener('click', function () {
+    playRound(2, computerPlay())
+})
+document.getElementById("scissors").addEventListener('click', function () {
+    playRound(1, computerPlay())
+})
+document.getElementById("paper").addEventListener('click', function () {
+    playRound(0, computerPlay())
+})
 
-function displayRecord(wins, losses, ties) {
-    document.getElementById("record").innerHTML = `${wins}-${losses}-${ties}`
-}
 
-function resetGame() {
-    document.getElementById("results").innerHTML = "Results Shown Here:"
-    return
-}
+let playerWins = 0
+let computerWins = 0
 
-function playRound(playerSelection, computerSelection, wins, losses, ties) {
-    if (playerSelection.toLowerCase() === "rock") {
-        if (computerSelection === "Paper") {
-            result = 10
-        } else if (computerSelection === "Scissors"){
-            result = 11
-        } else {
-            result = 12
-        }
-    }
-    if (playerSelection.toLowerCase() === "paper") {
-        if (computerSelection === "Paper") {
-            result = 22
-        } else if (computerSelection === "Scissors") {
-            result = 20
-        } else {
-            result = 21
-        }
-    }
-    if (playerSelection.toLowerCase() === "scissors") {
-        if (computerSelection === "Paper") {
-            result = 31
-        } else if (computerSelection === "Scissors") {
-            result = 32
-        } else {
-            result = 30
-        }
-    }
-    if (result % 10 === 0) {
-        let message = `<br>Loss: The Computer picked ${computerSelection} which beats ${playerSelection}`
-        displayResult(message)
-    } else if (result % 10 === 1) {
-        let message = `<br>Win: The computer picked ${computerSelection} which loses to ${playerSelection}!`
-        displayResult(message)
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection > computerSelection && playerSelection !== computerSelection + 2) || playerSelection === computerSelection - 2) {
+        result = `You win! ${moves[playerSelection]} beats ${moves[computerSelection]}.`
+        playerWins++
+    } else if ((computerSelection > playerSelection && computerSelection !== playerSelection + 2) || computerSelection === playerSelection - 2) {
+        result = `You lose, ${moves[computerSelection]} beats ${moves[playerSelection]}.`
+        computerWins++
     } else {
-        let message = `<br>Tie: The Computer also picked ${computerSelection}`
-        displayResult(message)
+        result = `It's a tie, the computer also picked ${moves[computerSelection]}`
+    }
+    document.getElementById("results").innerHTML += `<p>${result}</p>`
+    document.getElementById("score").innerHTML = playerWins + '-' + computerWins
+    if (playerWins === 5) {
+        document.getElementById("results").innerHTML += `<p>Good job, you won!</p>`
+        document.getElementById("rock").remove()
+        document.getElementById("scissors").remove()
+        document.getElementById("paper").remove()
+    } else if(computerWins === 5) {
+        document.getElementById("results").innerHTML += `<p>You lost, better luck next time.</p>`
+        document.getElementById("rock").remove()
+        document.getElementById("scissors").remove()
+        document.getElementById("paper").remove()
     }
 }
-
-function game() {
-    document.getElementById("rock").onclick = function () { playRound('Rock', computerPlay()) }
-    document.getElementById("paper").onclick = function () { playRound('Paper', computerPlay()) }
-    document.getElementById("scissors").onclick = function () { playRound('Scissors', computerPlay()) }
-    document.getElementById("reset").onclick = function () { resetGame() }
-}
-game()
